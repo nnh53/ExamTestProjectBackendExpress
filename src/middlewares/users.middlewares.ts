6c59c32aeca8aae02d38e97df04171040dc9ef5e
quyenpq.work@gmail.com <quyenpq.work@gmail.com>
2023-11-01 16:24:32 +0700

    submit route

--- src/middlewares/users.middlewares.ts
+++ src/middlewares/users.middlewares.ts
@@ -54,3 +54,48 @@ export const registerValidator = validate(
     }
   })
 )
+
+export const submitValidator = validate(
+  checkSchema(
+    {
+      name: {
+        notEmpty: {
+          errorMessage: USERS_MESSAGES.NAME_IS_REQUIRED
+        },
+        isString: {
+          errorMessage: USERS_MESSAGES.NAME_MUST_BE_A_STRING
+        },
+        trim: true,
+        isLength: {
+          options: {
+            min: 1,
+            max: 100
+          },
+          errorMessage: USERS_MESSAGES.NAME_LENGTH_MUST_BE_FROM_1_TO_100
+        }
+      },
+      email: {
+        notEmpty: {
+          errorMessage: USERS_MESSAGES.EMAIL_IS_REQUIRED
+        },
+        isEmail: {
+          errorMessage: USERS_MESSAGES.EMAIL_IS_INVALID
+        },
+        trim: true
+      },
+      test_id: {
+        notEmpty: {
+          errorMessage: USERS_MESSAGES.TEST_ID_IS_REQUIRED
+        },
+        trim: true
+      },
+      user_info_token: {
+        notEmpty: {
+          errorMessage: USERS_MESSAGES.USER_INFO_TOKEN_IS_REQUIRED
+        },
+        trim: true
+      }
+    },
+    ['body']
+  )
+)
